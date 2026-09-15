e1df569be6a912cc8f005e4b3ad43c03c30fea4e
zikXcodes <iizzzairy@gmail.com>
2026-09-15 14:32:21 +0800

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/zikXcodes/zik/refs/heads/main/release"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/zikXcodes/zik/refs/heads/main/Speed%20Hub%20X%205.0.0%20Ui%20Source%20Code.txt"))()
